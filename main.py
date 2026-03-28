56b6de67ff92e52d422e2d49eba3d50c4661aee6
asmnan <nandiasmita002@gmail.com>
2026-03-28 23:22:24 +0100

    Finish asteroids game

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
-from logger import log_state
+import sys
+from logger import log_state, log_event
+from player import Player
 import pygame
+from shot import Shot
 #from module_name import my_function
+from asteroid import Asteroid
+from asteroidfield import AsteroidField 
 from constants import *
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\n Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60) / 1000
+        #print(dt)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 if __name__ == "__main__":
     main()
